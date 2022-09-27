915687e92df8861139ec35483dc4e82022f1ecc9
root <root@localhost.localdomain>
2022-09-27 05:58:47 -0400

    adding test.py

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World!\n'
  
 # main driver function
 if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
+    
+   
+
